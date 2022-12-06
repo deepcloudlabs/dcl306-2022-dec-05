3c1c16f9dad9e890bb62f470ef1063636ef2b2d4
Binnur KURT <deepcloudlabs@gmail.com>
2022-12-06 10:41:17 +0300

    Update form-group.js

--- mastermind/src/component/form-group.js
+++ mastermind/src/component/form-group.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function FormGroup({show,children}){
+export default function FormGroup({show=true,children}){
     if(!show)
         return "";
     return(
